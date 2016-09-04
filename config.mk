1654efe1d67d0bec010792e8c6eb64a0f8e88a58
Ali H. Fardan <raiz@firemail.cc>
2016-09-05 00:56:22 +0300

    config.mk cleanup

--- config.mk
+++ config.mk
@@ -3,9 +3,7 @@ VERSION = 1.0
 
 # Customize below to fit your system
 
-# paths
 PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
@@ -17,13 +15,7 @@ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lasound
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_GNU_SOURCE
 CFLAGS = -std=c99 -pedantic -Wno-unused-function -Wall -Wextra -O0 ${INCS} ${CPPFLAGS}
-#CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = ${LIBS}
-#LDFLAGS = -s ${LIBS}
-
-# Solaris
-#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
-#LDFLAGS = ${LIBS}
 
 # compiler and linker
 CC = cc
