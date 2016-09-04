b9b6486cda91051553c9f9d7be75468c64206eec
Ali H. Fardan <raiz@firemail.cc>
2016-09-05 01:40:47 +0300

    bringed back the old config.mk

---
+++ config.mk
@@ -0,0 +1,19 @@
+VERSION = 1.0
+
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+INCS = -I. -I/usr/include -I${X11INC}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lasound
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_GNU_SOURCE
+CFLAGS = -std=c99 -pedantic -Wno-unused-function -Wall -Wextra -O0 ${INCS} ${CPPFLAGS}
+LDFLAGS = ${LIBS}
+
+CC = cc
+LD = ld
+
