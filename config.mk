d1423010d0832e6da3c11bea63e5f5299a21710b
Aaron Marcher <info@nulltime.net>
2016-09-18 17:22:12 +0200

    config.mk comment about -Wno-unused-functions

--- config.mk
+++ config.mk
@@ -1,3 +1,5 @@
+# See LICENSE file for copyright and license details.
+
 VERSION = 2.0
 
 PREFIX = /usr/local
@@ -9,11 +11,10 @@ X11LIB = /usr/X11R6/lib
 INCS = -I. -I/usr/include -I${X11INC}
 LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lasound
 
-# flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_GNU_SOURCE
+# -Wno-unused-function for routines not activated by user
 CFLAGS = -std=c99 -pedantic -Wno-unused-function -Wall -Wextra -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = ${LIBS}
 
 CC = cc
 LD = ld
-
