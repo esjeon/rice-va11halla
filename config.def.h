fcc5c683a6cb6efa380d82e6af34b31181ea1751
Aaron Marcher <me@drkhsh.at>
2017-08-14 14:04:21 +0200

    sexy config.def.h table

--- config.def.h
+++ config.def.h
@@ -9,41 +9,44 @@ static const char unknown_str[] = "n/a";
 /* maximum output string length */
 #define MAXLEN 2048
 
-/* statusbar
-- battery_perc (battery percentage) [argument: battery name]
-- battery_power (battery power usage) [argument: battery name]
-- battery_state (battery charging state) [argument: battery name]
-- cpu_perc (cpu usage in percent) [argument: NULL]
-- cpu_freq (cpu frequency) [argument: NULL]
-- datetime (date and time) [argument: format]
-- disk_free (free disk space in GB) [argument: mountpoint]
-- disk_perc (disk usage in percent) [argument: mountpoint]
-- disk_total (total disk space in GB) [argument: mountpoint]
-- disk_used (used disk space in GB) [argument: mountpoint]
-- entropy (available entropy) [argument: NULL]
-- gid (gid of current user) [argument: NULL]
-- hostname [argument: NULL]
-- ip (ip address) [argument: interface]
-- kernel_release (uname -r) [argument: NULL]
-- keyboard_indicators (caps/num lock indicators) [agrument: NULL]
-- load_avg (load average) [argument: NULL]
-- num_files (number of files in a directory - hint: maildir) [argument: path]
-- ram_free (free ram in GB) [argument: NULL]
-- ram_perc (ram usage in percent) [argument: NULL]
-- ram_total (total ram in GB) [argument: NULL]
-- ram_used (used ram in GB) [argument: NULL]
-- run_command (run custom shell command) [argument: command]
-- swap_free (free swap in GB) [argument: NULL]
-- swap_perc (swap usage in percent) [argument: NULL]
-- swap_total (total swap in GB) [argument: NULL]
-- swap_used (used swap in GB) [argument: NULL]
-- temp (temperature in celsius) [argument: temperature file]
-- uid (uid of current user) [argument: NULL]
-- uptime (uptime) [argument: NULL]
-- username (username of current user) [argument: NULL]
-- vol_perc (oss/alsa volume status (see README)) [argument: /dev/mixer]
-- wifi_perc (wifi signal in percent) [argument: wifi card interface name]
-- wifi_essid (wifi essid) [argument: wifi card interface name] */
+/*
+ * function             description                     argument
+ *
+ * battery_perc         battery percentage              battery name
+ * battery_power        battery power usage             battery name
+ * battery_state        battery charging state          battery name
+ * cpu_perc             cpu usage in percent            NULL
+ * cpu_freq             cpu frequency in MHz            NULL
+ * datetime             date and time                   format string
+ * disk_free            free disk space in GB           mountpoint path
+ * disk_perc            disk usage in percent           mountpoint path
+ * disk_total           total disk space in GB          mountpoint path
+ * disk_used            used disk space in GB           mountpoint path
+ * entropy              available entropy               NULL
+ * gid                  GID of current user             NULL
+ * hostname             hostname                        NULL
+ * ip                   IPv4 address                    interface name
+ * kernel_release       `uname -r`                      NULL
+ * keyboard_indicators  caps/num lock indicators        NULL
+ * load_avg             load average                    NULL
+ * num_files            number of files in a directory  path
+ * ram_free             free memory in GB               NULL
+ * ram_perc             memory usage in percent         NULL
+ * ram_total            total memory size in GB         NULL
+ * ram_used             used memory in GB               NULL
+ * run_command          custom shell command            command
+ * swap_free            free swap in GB                 NULL
+ * swap_perc            swap usage in percent           NULL
+ * swap_total           total swap size in GB           NULL
+ * swap_used            used swap in GB                 NULL
+ * temp                 temperature in degree celsius   sensor file
+ * uid                  UID of current user             NULL
+ * uptime               system uptime                   NULL
+ * username             username of current user        NULL
+ * vol_perc             OSS/ALSA volume in percent      "/dev/mixer"
+ * wifi_perc            WiFi signal in percent          interface name
+ * wifi_essid           WiFi ESSID                      interface name
+ */
 static const struct arg args[] = {
 	/* function format          argument */
 	{ cpu_perc, "[CPU %3s%%] ", NULL    },
