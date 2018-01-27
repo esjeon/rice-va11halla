f392e00a6ff19637b768a6154d2013ee87b64443
Eon S. Jeon <esjeon@hyunmu.am>
2018-01-27 16:49:55 +0900

    implement battery_state_fa

--- slstatus/components/battery.c
+++ slstatus/components/battery.c
@@ -54,3 +54,31 @@ battery_state(const char *bat)
 	}
 	return (i == LEN(map)) ? "?" : map[i].symbol;
 }
+
+const char *
+battery_state_fa(const char *bat)
+{
+	struct {
+		char *state;
+		char *symbol;
+	} map[] = {
+		{ "Charging",    "\xef\x89\x82" },
+		{ "Discharging", "\xef\x83\xa7" },
+		{ "Full",        "\xef\x87\xa6" },
+		{ "Unknown",     "\xef\x81\x9e" },
+	};
+	size_t i;
+	char path[PATH_MAX], state[12];
+
+	snprintf(path, sizeof(path), "%s%s%s", "/sys/class/power_supply/", bat, "/status");
+	if (pscanf(path, "%12s", state) != 1) {
+		return NULL;
+	}
+
+	for (i = 0; i < LEN(map); i++) {
+		if (!strcmp(map[i].state, state)) {
+			break;
+		}
+	}
+	return (i == LEN(map)) ? "?" : map[i].symbol;
+}
--- slstatus/slstatus.h
+++ slstatus/slstatus.h
@@ -4,6 +4,7 @@
 const char *battery_perc(const char *);
 const char *battery_power(const char *);
 const char *battery_state(const char *);
+const char *battery_state_fa(const char *);
 
 /* cpu */
 const char *cpu_freq(void);
